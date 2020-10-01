80139bf29e893219c80810365786b271cd4d6ad3
jangle <shubhamjangale0000@gmail.com>
2020-10-01 13:04:50 +0530

    This Program gives the ability to add Multiple persons to the AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -1,18 +1,7 @@
 import java.util.*;
 
 public class AddressBook {
-	public String name;
-	public AddressBook(String name) {
-		this.name=name;
-	}
-	public Contact contact;
 	public ArrayList<Contact> addressBook = new ArrayList<Contact>();
-	public Contact getContact() {
-		return contact;
-	}
-	public void setContact(Contact contact) {
-		this.contact = contact;
-	}
 	public ArrayList<Contact> getAddressBook() {
 		return addressBook;
 	}
@@ -24,10 +13,10 @@ public class AddressBook {
 	}
 	public void editContact(String name) {
 		Scanner sc = new Scanner(System.in);
-		String editName = "";
+		String x = "";
 		for(Contact contact : addressBook) {
-			editName = contact.getFirstName() + contact.getLastName();
-			if(name.equals(editName)) {
+			x = contact.getFirstName() + contact.getLastName();
+			if(name.equals(x)) {
 				System.out.println("1.Change the address");
 				System.out.println("2.Change the city");
 				System.out.println("3.Change the state");
@@ -66,15 +55,15 @@ public class AddressBook {
 				}	
 			}
 			else {
-				System.out.println("No such contact exist");
+				 System.out.println("No such contact exist");
 			}
 		}
 	}
 	public void deleteContact(String name) {
-		String deleteName = "";
+		String x = "";
 		for(Contact contact : addressBook) {
-			deleteName = contact.getFirstName() + contact.getLastName();
-			if(name.equals(deleteName)) {
+			x = contact.getFirstName() + contact.getLastName();
+			if(name.equals(x)) {
 				addressBook.remove(contact);
 			}
 		}
