f83b1a7551bcbec8895b9af705599cd4a7949107
jangle <shubhamjangale0000@gmail.com>
2020-10-01 10:29:33 +0530

    This Program gives ability to Create Contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,7 @@
 public class AddressBookMain{
 	
     public static void main(String[] args) {
-	    System.out.println("Welcome to Address Book");
+	    System.out.println("Welcome to Address Book. This program gives the ability to create contact");
 	}
 
 }
